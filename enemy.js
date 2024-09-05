710a09a5881778d934126da62b8e061f361496a5
greyquail <greyquail56@gmail.com>
2024-09-05 15:13:02 +0000

    enemy.js

--- enemy.js
+++ enemy.js
@@ -6,7 +6,11 @@ class Enemy extends Phaser.GameObjects.Sprite {
         this.scene = scene;
         this.health = 100;
         this.speed = 50;
-          update(time, delta) {
+        this.x = 0;
+        this.y = Phaser.Math.RND.between(0, this.scene.sys.canvas.height);
+    }
+
+    update(time, delta) {
         // Update enemy logic here
         this.x += this.speed * delta;
         if (this.x > this.scene.sys.canvas.width) {
